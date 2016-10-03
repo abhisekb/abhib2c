84ff0683bcd63c678a53bb9096981de042036b49
Abhisek Banerjee <abhisek_98@yahoo.com>
2016-10-03 07:55:34 +0530

    Added login page

---
+++ WebApp-B2C-DotNet/Controllers/UserAccountController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebApp_OpenIDConnect_DotNet_B2C.Controllers
+{
+    public class UserAccountController : Controller
+    {
+        // GET: UserAccount
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        public ActionResult Signup()
+        {
+            return View();
+        }
+    }
+}
